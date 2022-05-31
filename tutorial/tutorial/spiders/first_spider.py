dce3aefa788dcf4c158d6f0b384eeb7f6d815305
Wes <wal2.designer@gmail.com>
2022-05-31 11:34:33 +0100

    start scrapy project

---
+++ tutorial/tutorial/spiders/first_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class QautesSpider(scrapy.Spider):
+    name = 'quotes'
+
+    def start_request(self):
+        urls = [
+            'https://quotes.toscrape.com/page/1',
+            'https://quotes.toscrape.com/page/2',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, resposen):
+        page = response.url.split('/')[-2]
+        filename = f'quotes={page}.html'
+
+        with open(filename, 'wp') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
