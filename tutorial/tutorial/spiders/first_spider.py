1c4dcacf24cf1a3245ce16b2d86e1ad8067d926f
Wes <wal2.designer@gmail.com>
2022-05-31 11:50:50 +0100

    first scrapy

--- tutorial/tutorial/spiders/first_spider.py
+++ tutorial/tutorial/spiders/first_spider.py
@@ -6,13 +6,13 @@ class QautesSpider(scrapy.Spider):
 
     def start_request(self):
         urls = [
-            'https://quotes.toscrape.com/page/1',
-            'https://quotes.toscrape.com/page/2',
+            'https://quotes.toscrape.com/page/1/',
+            'https://quotes.toscrape.com/page/2/',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
-    def parse(self, resposen):
+    def parse(self, response):
         page = response.url.split('/')[-2]
         filename = f'quotes={page}.html'
 
